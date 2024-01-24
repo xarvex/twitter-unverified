26cc7e291087dd009616de588bc6934e76996f72
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-01-24 15:11:26 -0600

    Comment out leftover test code

--- inject.js
+++ inject.js
@@ -5,8 +5,13 @@ script.onload = function() {
     this.remove();
 };
 
+/* TODO
+ * use user in local storage to filter out tweets on initial page load,
+ * which does not get intercepted by xhr.js
+ *
 window.addEventListener("xarvex/twitter-unverified/UserHidden", function(event) {
     if (event.target === window)
         console.log(event.detail);
 });
+*/
 (document.head ?? document.documentElement).appendChild(script);
